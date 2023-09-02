c8bd326b989d757ba18f4a223a02c9694fa93e1b
ramamimu <ahhmaad.blogger@gmail.com>
2023-09-03 01:19:47 +0700

    feat: custom action, slot, and form

--- actions/actions.py
+++ actions/actions.py
@@ -7,10 +7,10 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 #
 #
 # class ActionHelloWorld(Action):
@@ -26,7 +26,7 @@
 #
 #         return []
 
-from rasa_sdk.interfaces import Action
+# from rasa_sdk.interfaces import Action, Tracker
 
 class ActionCustom(Action):
     def name(self):
@@ -34,4 +34,26 @@ class ActionCustom(Action):
 
     def run(self, dispatcher, tracker, domain):
         dispatcher.utter_message("Hello World!")
+        return []
+    
+# class ActionName(Action):
+#     def name(self):
+#         return "action_name"
+
+#     def run(self, dispatcher, tracker, domain):
+#         dispatcher.utter_message("My name is Rasa!")
+#         return []
+
+class ActionShoe(Action):
+    def name(self):
+        return "action_shoe"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain):
+        shoe_size = tracker.get_slot("shoesize")
+
+        if not shoe_size:
+            dispatcher.utter_message("Please provide your shoe size!")
+            return []
+        else:
+            dispatcher.utter_message("Your shoe size is {}".format(shoe_size))
         return []
